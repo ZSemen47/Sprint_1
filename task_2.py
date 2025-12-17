2cce6bacdfc3c588253cd2c26f4508453dea3d17
Zhao Sen <zazulinsemen@gmail.com>
2025-12-18 00:06:22 +0300

    task_2 fix my comment

--- task_2.py
+++ task_2.py
@@ -5,7 +5,7 @@ class Tester:
         self.deadline = deadline
 
     def work_hard(self, deadline):
-        self.deadline = deadline #д
+        self.deadline = deadline #добавляем возможность задать значение атрибута при вызове метода
         if self.deadline:
             print(self.name, 'Что ж, ещё часок поработаю!')
         else:
